db84776dc108ec3e5e8d7bad08f8682a5e23ae9a
Dmitri Akatov <akatov@gmail.com>
2017-07-14 14:15:09 +0100

    exit with code 1 on error

--- cli.js
+++ cli.js
@@ -12,7 +12,10 @@ var url = args[0]
 var source = args[1] || process.cwd()
 
 configure(url, source, function (error, response) {
-  if (error) return console.error(error)
+  if (error) {
+    console.error(error)
+    process.exit(1)
+  }
 
   console.log(JSON.stringify(response, null, '  '))
 })
